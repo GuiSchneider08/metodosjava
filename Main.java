f09121424b723824bfd0e1af6ecb9bd2f967ed62
unknown <guilhermelactodama@gmail.com>
2022-02-14 15:27:40 -0300

    first commit

---
+++ Main.java
@@ -0,0 +1,29 @@
+package calculadora;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		// Calculadora2
+		System.out.println("Exercicio calculadora");
+		Calculadora2.soma(3, 6);
+		Calculadora2.subtracao(9, 1.8);
+		Calculadora2.multiplicacao(7, 8);
+		Calculadora2.divisao(5, 2.5);
+		
+		
+		// Mensagem
+		System.out.println("Exercicio Mensagem");
+		Mensagem.obterMensagem(9);
+		Mensagem.obterMensagem(15);
+		Mensagem.obterMensagem(24);
+		
+			
+		//Empréstimo
+		System.out.println("Exercicio emprestimo");
+		Emprestimo.calcular(1000, Emprestimo.getDuasParcelas());
+		Emprestimo.calcular(1000, );
+		Emprestimo.calcular(valor: 1000, parcelas: 5); 	
+	}
+
+}
